ea3986ff00a8fc715a5143992a0bcc2ef906ba5e
jnkue <janik.ue@gmail.com>
2022-09-06 16:41:08 +0200

    added doctype

---
+++ fluxa/fluxen/doctype/firmware/firmware.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Janik Uellendahl and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Firmware', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ fluxa/fluxen/doctype/firmware/test_firmware.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Janik Uellendahl and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFirmware(FrappeTestCase):
+	pass
