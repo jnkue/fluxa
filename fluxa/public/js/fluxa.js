ae016e590879293721fd98181fc581a41fdf7160
jnkue <janik.ue@gmail.com>
2022-09-09 13:16:14 +0200

    automatic marking for deveopment

--- fluxa/public/js/fluxa.js
+++ fluxa/public/js/fluxa.js
@@ -1,16 +1,32 @@
-document.addEventListener("DOMContentLoaded", function(event) {
 
-console.log("Put a message here.");
-var elements = document.getElementsByClassName("container");
-console.log(elements.length);
-for(var i=0;i<elements.length;i++)
-{
-  elements[i].innerHTML = 'Testing here';
-  console.log("Put a message here.");
-}});
+function get_branch() {
+        return new Promise((resolve, reject) => {
+            frappe.call({
+                method: "frappe.utils.change_log.get_versions",
+                callback: function (r) {
+                    versions = r.message;
+                    app_branch = versions['fluxa']['branch']
+                     resolve(app_branch)
+                }
+            })
+        })
+      }
 
-document.addEventListener("DOMContentLoaded", function(e){
-    var slides = document.getElementsByClassName("navbar");
-  console.log(slides.length); 
-  console.log(slides[0]); 
-  });
+async function changebanner_development(){
+    const app_branch = await get_branch()
+    console.log(app_branch);
+
+    if (app_branch == 'development') 
+    {
+        // Change text of logo
+        var elements = document.getElementsByClassName('navbar-brand navbar-home');
+        Array.prototype.forEach.call(elements, function(element) {
+        element.innerText = 'Development Enviroment';  
+        });
+        // Change background
+        var elements = document.getElementsByClassName('navbar');
+        Array.prototype.forEach.call(elements, function(element) {
+        element.style.background = '#17FFC3' });
+    }};
+
+changebanner_development();
